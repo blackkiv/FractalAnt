8a896560ed6f93a35ae9fa6ae1feb0c64ef91436
blackkiv <fallena238@gmail.com>
2021-02-09 00:40:36 +0200

    Добавьте файлы проекта.

---
+++ FractalAnt/MainWindow.xaml.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FractalAnt
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private Thread thread;
+
+        private List<List<Rectangle>> rectangles = null;
+        Rectangle currRectangle = null;
+        int currColumn;
+        int currRow;
+        int direction;  // 0 - top;
+                        // 1 - right
+                        // 2 - bottom
+                        // 3 - left
+
+        private void Painter()
+        {
+            SolidColorBrush colorBrush = null;
+            Dispatcher.Invoke(() =>
+            {
+                colorBrush = ((SolidColorBrush)currRectangle.Fill);
+            
+                if (colorBrush.Color == Colors.Black)
+                {
+                    direction -= 1;
+                    if (direction < 0)
+                        direction = 3;
+                }
+                else
+                {
+                    direction += 1;
+                    if (direction > 3)
+                        direction = 0;
+                }
+            });
+            int columnK = 0;
+            int rowK = 0;
+            switch (direction)
+            {
+                case 0:
+                    rowK = -1;
+                    columnK = 0;
+                    break;
+                case 1:
+                    rowK = 0;
+                    columnK = 1;
+                    break;
+                case 2:
+                    rowK = 1;
+                    columnK = 0;
+                    break;
+                case 3:
+                    rowK = 0;
+                    columnK = -1;
+                    break;
+                default:
+                    MessageBox.Show("wtf?!?!");
+                    break;
+            }
+
+            currColumn += columnK;
+            currRow += rowK;
+            currRectangle = rectangles[currRow][currColumn];
+            Dispatcher.Invoke(() =>
+           {
+               if (colorBrush.Color == Colors.Black)
+                   colorBrush.Color = Colors.White;
+               else
+                   colorBrush.Color = Colors.Black;
+           });
+        }
+
+        private void Initialize_gridPanel()
+        {
+            gridPanel.ColumnDefinitions.Clear();
+            gridPanel.RowDefinitions.Clear();
+            var size = Convert.ToInt32(sizeTextBox.Text);
+
+            for (int i = 0; i < size; ++i)
+            {
+                gridPanel.ColumnDefinitions.Add(new ColumnDefinition());
+                gridPanel.RowDefinitions.Add(new RowDefinition());
+            }
+
+            rectangles = new List<List<Rectangle>>(size);
+
+            for (int i = 0; i < size; ++i)
+            {
+                var sublist = new List<Rectangle>(size);
+                for (int j = 0; j < size; ++j)
+                {
+                    var rectangle = new Rectangle();
+                    rectangle.MouseLeftButtonUp += Rectangle_Click;
+                    //rectangle.Stroke = new SolidColorBrush(Colors.Black);
+                    rectangle.Fill = new SolidColorBrush(Colors.White);
+                    //if ((i + j) % 2 == 0)
+                    //    rectangle.Fill = new SolidColorBrush(Colors.White);
+                    //else
+                    //    rectangle.Fill = new SolidColorBrush(Colors.Black);
+                    sublist.Add(rectangle);
+                    Grid.SetRow(rectangle, i);
+                    Grid.SetColumn(rectangle, j);
+
+                    gridPanel.Children.Add(rectangle);
+                }
+                rectangles.Add(sublist);
+            }
+        }
+
+        private void Rectangle_Click(object sender, RoutedEventArgs e)
+        {
+            Set_Start_Point(sender as Rectangle);
+        }
+
+        private void Set_Start_Point(Rectangle rectangle)
+        {
+            int column = Grid.GetColumn(rectangle);
+            int row = Grid.GetRow(rectangle);
+            //MessageBox.Show($"column = {column}\nrow = {row}");
+            currRectangle = rectangles[row][column];
+            currColumn = column;
+            currRow = row;
+            direction = 0;
+
+            thread = new Thread(Painter);
+            thread.Start();
+            
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StartBtn_Click(object sender, RoutedEventArgs e)
+        {
+            thread?.Abort();
+            Initialize_gridPanel();
+        }
+    }
+}
